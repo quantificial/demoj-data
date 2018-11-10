839b5d12424e450393acfb63c4b42ed8dcb539f3
Johnson Fu <johnson.fu@gmail.com>
2018-11-10 16:37:51 +0800

    remove bin

--- bin/main/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `book`;
-CREATE TABLE `book` (
-  `bookid` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `author` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`bookid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
