cd77a9d5306ef2f34c4abc1e7cdbc0c0c9c73bdf
Johnson Fu <johnson.fu@gmail.com>
2018-11-10 16:36:12 +0800

    initi

---
+++ src/main/java/com/quantificial/demojdata/Book.java
@@ -0,0 +1,17 @@
+package com.quantificial.demojdata;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue
+    private Integer bookid;
+    private String name;
+    private String author;
+}
---
+++ src/main/java/com/quantificial/demojdata/DemojDataApplication.java
@@ -0,0 +1,12 @@
+package com.quantificial.demojdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojDataApplication.class, args);
+	}
+}
